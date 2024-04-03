25eed2fa9053fe88c243fcbf4a1be788a897dec6
Martin Sjåvik <martinsjaavik@outlook.com>
2024-04-03 12:14:14 +0200

    lekt litt med gui

--- test2.py
+++ test2.py
@@ -30,7 +30,7 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
-        self.frame_left = customtkinter.CTkFrame(master=self, width=150, corner_radius=0, fg_color=None)
+        self.frame_left = customtkinter.CTkFrame(master=self, width=450, corner_radius=0, fg_color=None)
         self.frame_left.grid(row=0, column=0, padx=0, pady=0, sticky="nsew")
 
         self.frame_right = customtkinter.CTkFrame(master=self, corner_radius=0)
