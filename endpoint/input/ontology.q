a9925917174957e5de9751e6276ecb0eddf723a8
Martin Sjåvik <martinsjaavik@outlook.com>
2024-04-12 12:41:54 +0200

    changes

--- endpoint/input/ontology.q
+++ endpoint/input/ontology.q
@@ -3,3 +3,24 @@ SELECT ?s ?p ?o WHERE
 {
 	?s ?p ?o .
 }
+[QueryItem="Obesity rate"]
+PREFIX ex: <http://example.org/ontology/>
+
+PREFIX schema: <http://schema.org/>
+
+SELECT ?country ?o WHERE {
+        ?country ex:ObesityRate ?o.
+    }
+ORDER BY DESC(?o)
+LIMIT 50
+[QueryItem="Life expectancy"]
+PREFIX ex: <http://example.org/ontology/>
+
+PREFIX schema: <http://schema.org/>
+
+SELECT ?country ?LifeExpectancy WHERE {
+        ?country ex:belongsToContinent ex:Europe;
+		ex:hasLifeExpectancy ?LifeExpectancy.
+    }
+ORDER BY ASC(?country)
+LIMIT 50
--- test.py
+++ test.py
@@ -1,14 +1,39 @@
-from rdflib import Graph, Namespace, URIRef, BNode, Literal
+from rdflib import Graph
+from rdflib.plugins.sparql import prepareQuery
+
+# Load your ontology file into a RDFLib Graph
+g = Graph()
+g.parse("ontology-materialized.ttl", format="ttl")  # Replace with the actual path to your ontology file and format
+
+# Define your SPARQL query
+query = """
+    PREFIX ex: <http://example.org/ontology/>
+PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX xml: <http://www.w3.org/XML/1998/namespace>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX obda: <https://w3id.org/obda/vocabulary#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX schema: <http://schema.org/>
 
 
-def test():
-        g = Graph()
-        g.parse("ontology.ttl")
-        q = g.query("""
-SELECT DISTINCT ?s ?p ?o WHERE {
-?s ?p ?o .
-}
 
-""")
-        print(list(q))
-test()
+SELECT ?country ?o WHERE {
+        ?country ex:ObesityRate ?o.
+    }
+ORDER BY DESC(?o)
+LIMIT 50
+    """ # Adjust your query as per your ontology structure
+
+# Execute the SPARQL query
+results = g.query(query)
+
+# Process the results
+for row in results:
+    print(str(row[0]).split("/")[-1])
+    print(str(row[1]).split("/")[-1])
+
+# Close the graph
+g.close()
